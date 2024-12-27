e327944bc5be33e06d4108e4f3b8798165d28d6c
peichao.dong <peichao.dong@thoughtworks.com>
2024-12-27 17:10:40 +0800

    feat: add execute time log

--- src/main/java/cn/dpc/ecommerce/batch/listener/MyJobExecutionListener.java
+++ src/main/java/cn/dpc/ecommerce/batch/listener/MyJobExecutionListener.java
@@ -1,12 +1,29 @@
 package cn.dpc.ecommerce.batch.listener;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 
+import java.util.Optional;
+
+@Slf4j
 public class MyJobExecutionListener implements JobExecutionListener {
+
+    public static final String EXECUTION_START_TIME = "executionStartTime";
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        jobExecution.getExecutionContext().put(EXECUTION_START_TIME, System.currentTimeMillis());
+        log.info("before Job: {}", jobExecution.getJobInstance().getJobName());
+    }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
-        System.out.println("after Job:" + jobExecution.getJobInstance().getJobName());
+        long startTime = Optional.ofNullable(jobExecution.getExecutionContext().get(EXECUTION_START_TIME, Long.class)).orElse(0L);
+        if(startTime != 0) {
+            long endTime = System.currentTimeMillis();
+            log.info("Job: {} took {} ms", jobExecution.getJobInstance().getJobName(), endTime - startTime);
+        }
+        log.info("after Job: {}", jobExecution.getJobInstance().getJobName());
     }
 }
